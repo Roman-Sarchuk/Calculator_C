9f0fb72878bd183000582c1fa811a81052572c4d
Roman-Sarchuk <91730539+Roman-Sarchuk@users.noreply.github.com>
2022-01-21 13:44:19 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <Windows.h>
+#include <stdbool.h>
+
+void txt(float* x, char* operation, float* y);
+void logic(float x, char operation, float y);
+
+
+int main() {
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+
+	float x, y;
+	char oprt;
+
+	while (true) {
+		printf("--- Calculator ---\n");
+		txt(&x, &oprt, &y);
+		printf("| %f | %c | %f |\n", x, oprt, y);
+		logic(x, oprt, y);
+	}
+
+
+	return 0;
+}
+
+
+void txt(float* x, char* operation, float* y) {
+	printf("The first numder: ");
+	scanf("%f", x);
+	printf("Arithmetic operation: ");
+	scanf("%s", operation);
+	printf("The second numeric: ");
+	scanf("%f", y);
+}
+
+void logic(float x, char operation, float y) {
+	if (operation == '+')
+		printf("Result: %f\n\n", x + y);
+	else if (operation == '-')
+		printf("Result: %f\n\n", x - y);
+	else if (operation == '*')
+		printf("Result: %f\n\n", x * y);
+	else if (operation == '/')
+		if (y == 0)
+			printf("Result: ! На нуль ділити не можна \n\n!");
+		else
+			printf("Result: %f\n\n", x / y);
+}
